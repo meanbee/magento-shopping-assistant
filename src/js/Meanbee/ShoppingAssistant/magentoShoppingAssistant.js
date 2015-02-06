42a3ae85ecac4c4ccf4cbd3607a786e5e57267dd
Tom Robertshaw <me@tomrobertshaw.net>
2015-02-06 11:39:46 +0000

    Move together js ready listened to when button is clicked.
    - Formatting and commenting
    - Debug mode.

--- src/js/Meanbee/ShoppingAssistant/magentoShoppingAssistant.js
+++ src/js/Meanbee/ShoppingAssistant/magentoShoppingAssistant.js
@@ -1,41 +1,98 @@
 /** var document, TogetherJS **/
-;'use strict';
+/**
+ * This library wraps around TogetherJS in order to provide shopping assistant
+ * functionality to Magento.  It will listen for requests from start elements
+ * and send the request to Magento admin to be dealt with.
+ */
+;(function() {
+    'use strict';
 
-var Meanbee_ShoppingAssistant = Class.create();
-Meanbee_ShoppingAssistant.prototype = {
-    initialize: function(options) {
-        this.options = Object.extend({
-            startElements: "[data-shoppingassistant-start]",
-            started: false
-        }, options || {} );
+    var Meanbee_ShoppingAssistant = Class.create();
 
-        document.addEventListener('togetherjs:ready', this.notifyAdmin.bind(this));
+    Meanbee_ShoppingAssistant.prototype = {
+        initialize: function(options) {
+            this.options = Object.extend({
+                startElements: "[data-shoppingassistant-start]",
+                started: false,
+                debug: false
+            }, options || {} );
 
-        $$(this.options.startElements).each(function(element) {
-            Event.observe(element,'click', this.toggleSession.bind(this));
-        }.bind(this));
-    },
+            // Listen to buttons which can be used to request a session
+            $$(this.options.startElements).each(function(element) {
+                Event.observe(element,'click', this.toggleSession.bind(this));
+            }.bind(this));
 
-    toggleSession: function() {
-        TogetherJS(this);
-    },
+            // TogetherJS callback for a party joining the session, e.g. admin
+            TogetherJS.hub.on("togetherjs.hello", this.memberJoined.bind(this));
+        },
 
-    notifyAdmin: function() {
-        new Ajax.Request(MeanbeeBaseUrl + 'shopping_assistant/request/create', {
-            parameters: {
-                url: TogetherJS.shareUrl(),
-                name: TogetherJS.config.get("getUserName")(),
-                email: TogetherJSConfig_getEmail()
-            },
-            onSuccess: function(response) {
-                var json = response.responseJSON;
-                if (json.response) {
-                    $$(this.options.startElements).first().innerText = json.response;
-                }
+        /**
+         * Toggle debug mode
+         *
+         * @param enable
+         */
+        debug: function(enable) {
+            this.options.debug = enable;
+        },
+
+        /**
+         * Member has joined the shopping assistant session
+         */
+        memberJoined: function() {
+            this.log("Member joined.");
+            $$(this.options.startElements).first().innerText = "Assistant arrived.";
+        },
+
+        /**
+         * Start or stop shopping assistant session
+         */
+        toggleSession: function() {
+
+            if (!TogetherJS.running) {
+                this.log("Starting Shopping Assistant");
+                document.observe('togetherjs:ready', this.notifyAdmin.bind(this));
+            } else {
+                this.log("Ending Shopping Assistant")
             }
-        });
-    }
 
-};
+            TogetherJS(this);
+        },
 
-window.meanbee_shoppingassistant = new Meanbee_ShoppingAssistant();
+        /**
+         * Send shopping assistant request to admin
+         */
+        notifyAdmin: function() {
+            var self = this;
+
+            self.log("Sending request to admin");
+
+            new Ajax.Request(MeanbeeBaseUrl + 'shopping_assistant/request/create', {
+                parameters: {
+                    url: TogetherJS.shareUrl(),
+                    name: TogetherJS.config.get("getUserName")(),
+                    email: TogetherJSConfig_getEmail()
+                },
+                onSuccess: function(response) {
+                    var json = response.responseJSON;
+                    if (json.response) {
+                        $$(self.options.startElements).first().innerText = json.response;
+                    }
+                }
+            });
+        },
+
+        /**
+         * Log messages to console if debug mode enabled.
+         *
+         * @param message
+         */
+        log: function(message) {
+            if (this.options.debug) {
+                console.log(message);
+            }
+        }
+
+    };
+
+    window.Meanbee_ShoppingAssistant = new Meanbee_ShoppingAssistant();
+})();
